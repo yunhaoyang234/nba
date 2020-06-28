46f3ac89e0750f5e4081e52ae3b1a56958c3d0e5
yyh <yyh@Yunhaos-MacBook.local>
2020-06-28 16:43:17 -0500

    add

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import TopNavBar from './TopNavBar';
+import '../styles/App.css';
+import Main from "./main";
+
+function App() {
+  return (
+    <div className="App">
+      <TopNavBar />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
